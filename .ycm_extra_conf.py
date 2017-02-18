1302be959afe663a91fb8f4cd8692ae4d9270ad1
Maurice Bos <m-ou.se@m-ou.se>
2017-02-18 17:13:00 +0100

    Add ycm config.

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,13 @@
+import os
+
+dir = os.path.dirname(os.path.abspath(__file__))
+
+def FlagsForFile(filename, **kwargs):
+	return {
+		'flags': [
+			'-x', 'c++',
+			'-std=c++14',
+			'-I', dir + '/mstd/include',
+		],
+		'do_cache': True
+	}
